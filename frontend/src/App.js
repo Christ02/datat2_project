25059bba2a2231b13d9fd5b368d92e6c5761b9d0
Mayco Castellanos <103969414+Maik3000@users.noreply.github.com>
2024-08-25 20:29:09 -0600

    update db.js, server.js, app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import AdminPanel from './components/AdminPanel';
+import HealthDataEntry from './components/HealthDataEntry';
 
 function App() {
   return (
@@ -11,10 +14,17 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/health-data" element={<HealthDataEntry />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+
+
+
+
